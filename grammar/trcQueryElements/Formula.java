e6347b65eb5ecdde124179e0c499ebc0aaad25f2
Matheus Mayron <matheusmayron@gmail.com>
2016-12-07 09:03:05 -0300

    Arquivos que não pertecem mais ao projeto ou estão em subpastas

--- grammar/trcQueryElements/Formula.java
+++
@@ -1,7 +0,0 @@
-package trcQueryElements;
-import visitors.*;
-public abstract class Formula{
-	public abstract void accept(Visitor v);
-	public abstract Formula accept(VisitorFormula v);
-	public abstract String accept(VisitorString v);
-}
